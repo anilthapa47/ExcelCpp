99f619220d2bc1999977d25a8652d41f74d7f66d
anilthapa47 <35453811+anilthapa47@users.noreply.github.com>
2020-11-12 22:15:37 +0200

    class network added

--- STL.cpp
+++ STL.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
 using namespace std
+class Network{
+  private:
+ int networkcount_;
+  public:
+  Network();
+  int countNetwork()const{
+    networkcount_+=1;
+    return networkcount;
+  
+};
 int main()
 {
   cout<<"More stl of CPP"<<endl;
+  
 }
